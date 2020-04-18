3c1e544ebf8b08995414e8dee7e29dfbc8bb7c1e
Andrea <a.g.baldan@gmail.com>
2020-04-18 12:15:55 +0200

    Removed ref from vector, leverage inherited mutability

--- src/timeseries.rs
+++ src/timeseries.rs
@@ -1,6 +1,5 @@
 use std::thread::sleep;
 use std::option::Option;
-use std::cell::RefCell;
 use std::cmp::PartialEq;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
@@ -26,47 +25,46 @@ impl Record {
     }
 }
 
-pub struct TimeSeries<'a> {
+pub struct TimeSeries {
     name: String,
     retention: i64,
     ctime: u128,
-    records: RefCell<Vec<&'a Record>>
+    records: Vec<Record>
 }
 
-impl<'a> TimeSeries<'a> {
-    pub fn new(name: String, retention: i64) -> TimeSeries<'a> {
+impl TimeSeries {
+    pub fn new(name: String, retention: i64) -> TimeSeries {
         let ctime = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get now");
         TimeSeries {
             name: name,
             retention: retention,
             ctime: ctime.as_millis(),
-            records: RefCell::new(Vec::new())
+            records: Vec::new()
         }
     }
 
-    pub fn add_point(&self, r: &'a Record) {
-        self.records.borrow_mut().push(r);
+    pub fn add_point(&mut self, r: Record) {
+        self.records.push(r);
     }
 
     pub fn avg(&self) -> f64 {
         let a: f64 = self.records
-            .borrow()
             .iter()
             .map(|x| x.value)
-            .sum::<f64>() / self.records.borrow().len() as f64;
+            .sum::<f64>() / self.records.len() as f64;
         return a;
     }
 
     pub fn avg_interval(&self, interval: u128) -> Option<Vec<f64>> {
-        let first_ts = (self.records.borrow()[0].timestamp / interval) * interval;
-        match self.records.borrow().last() {
-            Some(last) => {
+        match self.records.first() {
+            Some(first) => {
+                let first_ts = (first.timestamp / interval) * interval;
+                let last = self.records.last().unwrap();
                 let last_ts = ((last.timestamp / interval) * interval) + interval;
                 let mut current_ts = first_ts + interval;
                 let mut avgs: Vec<f64> = Vec::new();
                 while current_ts <= last_ts {
                     let range: Vec<f64> = self.records
-                        .borrow()
                         .iter()
                         .filter(|v| v.timestamp > current_ts-interval
                             && v.timestamp < current_ts)
@@ -98,41 +96,41 @@ fn test_ts_new() {
 
 #[test]
 fn test_ts_add_point() {
-    let ts = TimeSeries::new("test-ts".to_string(), 0);
+    let mut ts = TimeSeries::new("test-ts".to_string(), 0);
     let r = Record::new(12.98);
-    ts.add_point(&r);
-    assert_eq!(ts.records.borrow().len(), 1);
-    assert_eq!(*ts.records.borrow()[0], r);
+    ts.add_point(r);
+    assert_eq!(ts.records.len(), 1);
+    assert_eq!(ts.records[0].value, 12.98);
 }
 
 #[test]
 fn test_ts_avg() {
-    let ts = TimeSeries::new("test-ts".to_string(), 0);
+    let mut ts = TimeSeries::new("test-ts".to_string(), 0);
     let r1 = Record::new(12.98);
     let r2 = Record::new(19.63);
     let r3 = Record::new(11.28);
     let r4 = Record::new(15.96);
-    ts.add_point(&r1);
-    ts.add_point(&r2);
-    ts.add_point(&r3);
-    ts.add_point(&r4);
+    ts.add_point(r1);
+    ts.add_point(r2);
+    ts.add_point(r3);
+    ts.add_point(r4);
     let avg = ts.avg();
     assert_eq!(avg, 14.9625);
 }
 
 #[test]
 fn test_ts_avg_interval() {
-    let ts = TimeSeries::new("test-ts".to_string(), 0);
+    let mut ts = TimeSeries::new("test-ts".to_string(), 0);
     let r1 = Record::new(12.98);
-    sleep(Duration::new(1, 0));
+    sleep(Duration::new(0, 5e8 as u32));
     let r2 = Record::new(19.63);
     let r3 = Record::new(11.28);
-    sleep(Duration::new(1, 0));
+    sleep(Duration::new(0, 5e8 as u32));
     let r4 = Record::new(15.96);
-    ts.add_point(&r1);
-    ts.add_point(&r2);
-    ts.add_point(&r3);
-    ts.add_point(&r4);
+    ts.add_point(r1);
+    ts.add_point(r2);
+    ts.add_point(r3);
+    ts.add_point(r4);
     let avg = ts.avg_interval(500 as u128).unwrap();
     assert_eq!(avg, [12.98, 15.454999999999998, 15.96]);
 }
